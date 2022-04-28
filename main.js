83cd699ef772e9c0b932090485f08189dc633409
Jordan Phillips <jordan.phillips.988@gmail.com>
2022-04-27 20:57:26 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const toggleNav = document.getElementById('toggleNav');
+const body = document.getElementById('body');
+const navList = document.getElementById('navList');
+
+
+toggleNav.addEventListener('click', function() {
+  body.classList.toggle('toggle-opacity');
+  navList.style.opacity = '1';
+})
