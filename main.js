bf53f49bc930a13e10dc660aea1987e8f32e1632
Jordan Phillips <jordan.phillips.988@gmail.com>
2022-04-29 21:13:49 -0600

    added thank you model

--- main.js
+++ main.js
@@ -2,15 +2,38 @@ const toggleNav = document.getElementById('toggleNav');
 const body = document.getElementById('body');
 const navList = document.getElementById('navList');
 
+const modalOpen = '[data-open]';
+const modalClose = '[data-close]';
+const dataPledge = '[data-pledge]';
+const openModal = document.querySelectorAll(modalOpen);
+const closeModal = document.querySelectorAll(modalClose);
+const pledgeContinue = document.querySelectorAll(dataPledge);
 
+// Toggles mobile nav
 toggleNav.addEventListener('click', function() {
   body.classList.toggle('toggle-opacity');
   navList.style.opacity = '1';
-})
+});
 
-const btn = document.getElementById('bamboo');
-const model = document.getElementById('back');
+// Opens pledge container on select reward
+for (const elm of openModal) {
+  elm.addEventListener('click', function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add('is-visible');
+  });
+}
 
-btn.addEventListener('click', function() {
-  model.classList.add('back-project--visible');
-})
+// Closes pledge container on X
+for (const elm of closeModal) {
+  elm.addEventListener('click', function() {
+    document.getElementById('selectReward').classList.remove('is-visible');
+  });
+}
+
+// Closes pledge container on continue
+for (const elm of pledgeContinue) {
+  elm.addEventListener('click', function(){
+    document.getElementById('selectReward').classList.remove('is-visible');
+  });
+}
+
